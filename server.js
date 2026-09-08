be38e97096a23115665ce5de0936516e5f5b711a
Tu Nombre <matehuala0484@gmail.com>
2026-09-08 16:11:43 -0600

    m

--- server.js
+++ server.js
@@ -49,13 +49,13 @@ createClient(
   SUPABASE_KEY
 );
 
-const SUPABASE_SERVICE_ROLE_KEY =
+const SUPABASE_SECRET_KEY =
   process.env.SUPABASE_SERVICE_ROLE_KEY;
 
 
 const supabaseAdmin = createClient(
   SUPABASE_URL,
-  SUPABASE_SERVICE_ROLE_KEY
+  SUPABASE_SECRET_KEY
 );
 
 // Mercado
